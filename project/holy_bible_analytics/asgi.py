6c4558355dec3e5773f7a928abc2a421a75e3b6c
Anselmo Lira <anselmo.lira@sinqia.com.br>
2025-04-17 18:41:00 -0300

    Adicionando projeto inicial Django

---
+++ project/holy_bible_analytics/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for holy_bible_analytics project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'holy_bible_analytics.settings')
+
+application = get_asgi_application()
